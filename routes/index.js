59e215dae2929abdc6c2c482ab275b9afd90d8f3
jamalludin <jamalludin1209@gmail.com>
2021-07-28 11:46:16 +0700

    fix ui

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Jamalludin Portfolio Website' });
+router.get('/', function (req, res, next) {
+    res.render('index',
+        {
+            title: 'Jamalludin Portfolio Website',
+            name: 'Jamalludin',
+            home_title: 'Hallo, Saya Jamalludin',
+            job_title: 'Backend Programmer',
+            introduce: 'Mempunyai Pengalaman bekerja 3 tahun sebagai Backend Programmer',
+            about_description: 'Backend Programmer Menggunakan Node.Js, JavaScript, Goolang'
+        });
 });
 
 module.exports = router
